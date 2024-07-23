f5a411849a9b77b18ffb33c968c1089c5849a5d0
Connor Carciofini <ccarciofini@gmail.com>
2024-07-22 23:03:26 -0500

    rm nodemodules

--- server/index.js
+++ server/index.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 6969;
-const path = require('path')
+const path = require('path');
 
-// app.get('/', (req, res) => {
-//     res.send('bet')
-// })
-
-app.use('/', express.static(path.join(__dirname, '../client')))
-app.listen(PORT, () => console.log(`SERVER SERVING AT PORT ${PORT}`))
+app.use('/', express.static(path.join(__dirname, '../client')));
+app.listen(PORT, () => console.log(`SERVER SERVING AT PORT ${PORT}`));
